1859e653aeb31ec8a51d0728867caea7dbf3ded0
Tech PC <techpc@techs-macbook-air.local>
2016-06-25 05:50:25 -0500

    Initial Commit

---
+++ redvsblue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  redvsblue
+//
+//  Created by Tech PC on 6/25/16.
+//  Copyright © 2016 Tech PC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
