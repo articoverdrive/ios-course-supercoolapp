ab951bb3737f77f1cbb80fd1872b2f050a6c0221
Tech PC <techpc@Techs-MacBook-Air.local>
2016-06-28 08:30:59 -0500

    Added files to Git Repository

--- redvsblue/ViewController.swift
+++ redvsblue/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    
+    @IBOutlet weak var hideBlue: UIButton!
+    
+    @IBOutlet weak var hideRed: UIButton!
+    
+    @IBOutlet weak var Restart: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideBlue(sender: AnyObject) {
+    blueBomb.hidden = true
+    }
 
+    @IBAction func hideRed(sender: AnyObject) {
+    redBomb.hidden = true
+    }
+    @IBAction func Restart(sender: AnyObject) {
+    blueBomb.hidden = false
+    redBomb.hidden = false
+    }
+    
 }
 
